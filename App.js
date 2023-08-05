09762904ddae69d506af92f9c8b885e51a33d4ae
mateen27 <khanmateen2713@gmail.com>
2023-08-05 21:26:58 +0530

    some modification in carousel images

--- App.js
+++ App.js
@@ -1,24 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-<<<<<<< HEAD
-import { StyleSheet, Text, View } from 'react-native';
-import Carousel from './components/Carousel';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Carousel/>
-      <StatusBar style="auto" />
-    </View>
-=======
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Logo from './components/Logo';
+import Carousel from './components/Carousel';
 
 export default function App() {
   return (
     <SafeAreaView>
       <Logo/>
+      <Carousel/>
     </SafeAreaView>
->>>>>>> 7a6774a4d408bb100813816f5205d697c216646f
   );
 }
 
